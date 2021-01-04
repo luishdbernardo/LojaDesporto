400ad55aca4b16a3ded8f1e18cca414b926bb691
Luís Bernardo <luishdbernardo@hotmail.com>
2021-01-04 15:29:43 +0000

    Cumprimentar o utilizador segundo a hora.

--- LojaDesporto/Controllers/HelloWorldController.cs
+++ LojaDesporto/Controllers/HelloWorldController.cs
@@ -10,6 +10,28 @@ namespace LojaDesporto.Controllers
     {
         public IActionResult Index()
         {
+
+            int hora = DateTime.Now.Hour; //função que dá a hora atual do sistema
+
+            string mensagem;
+
+            if(hora >= 7 && hora < 12)
+            {
+                mensagem = "Bom dia";
+            }
+            else if(hora >= 12 && hora < 20)
+            {
+                mensagem = "Boa tarde";
+            }
+            else
+            {
+                mensagem = "Boa noite";
+            }
+
+            //passar isto para a vista no index.cshtml:
+
+            ViewBag.Mensagem = mensagem;
+
             return View();
         }
     
