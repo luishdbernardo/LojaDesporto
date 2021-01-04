d09b7c9c8d093865dd437d35772e2a571ee8e22d
Luís Bernardo <luishdbernardo@hotmail.com>
2021-01-04 15:19:25 +0000

    Add project files.

---
+++ LojaDesporto/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LojaDesporto.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
